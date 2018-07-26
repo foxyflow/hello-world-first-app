ea34f7cbde65c8b9f9c3134e73fa69513a28fb64
Luke Fox <30011364@student.toiohomai.ac.nz>
2018-07-26 14:16:08 +1200

    please work

---
+++ hello world/hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("hello_world people. how long can this sentence be ");
+            Console.Read();
+
+            
+        }
+    }
+
+       
+}
